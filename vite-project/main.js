3db3d729e88fe79eba1884705a40f75fea3b2516
unknown <djimondo>
2023-06-19 15:48:16 -0400

    Challenge 2 complete

--- vite-project/main.js
+++ vite-project/main.js
@@ -6,8 +6,10 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 let scene = new THREE.Scene();
-let gui, mixer, actions, activeAction, loader, model, model2, animations;
+let gui, mixer, actions, activeAction, loader, actor1, actor2, animations, areFacingEachother;
 let loggedWorldCoordinates = false;
+//remove
+areFacingEachother = true;
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
 const renderer = new THREE.WebGLRenderer({
@@ -15,12 +17,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 const controls = new OrbitControls( camera, renderer.domElement);
 controls.enableDamping = true;
-controls.minDistance = 4;
+controls.minDistance = 2.5;
 controls.maxDistance = 10;
 controls.minAzimuthAngle = - Math.PI / 2;
 controls.maxAzimuthAngle = Math.PI / 2;
 controls.maxPolarAngle = Math.PI / 1.8;
-controls.target.set( 0, 1, 0 );
+controls.target.set( 0, 1, 2);
 
 
 renderer.setPixelRatio( window.devicePixelRatio );
@@ -28,7 +30,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 camera.position.set( -1, 1.5, 5 );
-camera.lookAt( 0, 1, 0 );
+camera.lookAt( 0, 1, 2 );
 
 scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xe0e0e0 );
@@ -60,55 +62,62 @@ scene.add( grid );
 window.addEventListener( 'resize', onWindowResize );
 
 
-//load model
-await loadModels();
-createGUI(model, animations);
+//load actor1
+await loadactor1s();
+createGUI(animations);
 
 
 
 animate();
 
-function animate(){
+function animate() {
   const dt = clock.getDelta();
   if ( mixer ) mixer.update( dt );
   //at 9 seconds log the world coordinates
-  if(clock.elapsedTime > 1 && !loggedWorldCoordinates){
-    console.log("vertex position at time: " + clock.elapsedTime + " : (" + model.position.x + ", " + model.position.y + ", " + model.position.z + ")");
+  if(clock.elapsedTime > 1 && !loggedWorldCoordinates) {
+    console.log("vertex position at time: " + clock.elapsedTime + " : (" + actor1.position.x + ", " + actor1.position.y + ", " + actor1.position.z + ")");
     loggedWorldCoordinates = true;
   }
+
+  if(areActorsFacingEachother()) {
+    actor2.material.color.set(0x28bd32);
+  }
+  else {
+    actor2.material.color.set(0x8f0000);
+  }
+
   requestAnimationFrame( animate );
   renderer.render(scene, camera);
   
 }
 
-//loads in the models needed for the scene
-async function loadModels() {
+//loads in the actor1s needed for the scene
+async function loadactor1s() {
   loader = new GLTFLoader();
   let gltf = await loader.loadAsync("ed.glb");
-  model = gltf.scene.children[0];
-  //model.position.set(0, 0, 0)
+  actor1 = gltf.scene.children[0];
+  //actor1.position.set(0, 0, 0)
   animations = gltf.animations;
-  scene.add(model);
+  scene.add(actor1);
 
   gltf = await loader.loadAsync("glozface.glb");
-  model2 = gltf.scene.children[0];
-  console.log(model2);
-  model2.scale.set(.15, .15, .15);
-  model2.position.set(1, 0, 0)
-  //scene.add(model2);
+  actor2 = gltf.scene.children[0];
+  actor2.scale.set(.15, .15, .15);
+  actor2.position.set(2, 0, 2);
+  scene.add(actor2);
 
 }
 
 //generates the gui and sets up the animationMixer
-function createGUI(model, animations){
+function createGUI( animations) {
   gui = new GUI();
-  mixer = new THREE.AnimationMixer( model );
+  mixer = new THREE.AnimationMixer( actor1 );
 
 	actions = {};
-  let ed = model.getObjectByName("ED2");
+  let ed = actor1.getObjectByName("ED2");
   //change from expressions to singular tense
-  const expressionFolder = gui.addFolder('Expressions');
-  expressionFolder.add( ed.morphTargetInfluences, 0, 0, 1, 0.01).name( 'talking shape key' );
+  gui.add( ed.morphTargetInfluences, 0, 0, 1, 0.01).name( 'talking shape key' );
+  gui.add(actor2.rotation, 'y', 0.01, 2* Math.PI);
   const clip = animations[ 0 ];
   activeAction = mixer.clipAction( clip );
   activeAction.play();
@@ -121,4 +130,39 @@ function onWindowResize() {
 
   renderer.setSize( window.innerWidth, window.innerHeight );
 
+}
+
+/* calculates if actor1 and actor2 are facing eachother
+ * Achieved by taking the dot product of each actor with
+ * the relativeDirection vector of the two actors
+ * using the result of this dot product it can be determined
+ * if the two actors are facing eachother, as if actor1 is facing
+ * toward the relative direction from actor1 to actor2 and actor2 is
+ * facing away from it (i.e. toward actor1) then it stands that the two
+ * actors are facing eachother
+ */
+function areActorsFacingEachother() {
+  const direction1 = new THREE.Vector3();
+  actor1.getWorldDirection(direction1);
+
+  const direction2 = new THREE.Vector3();
+  actor2.getWorldDirection(direction2);
+
+  //Calculate relative direction of the actors positions
+  const relativeDirection = new THREE.Vector3();
+  relativeDirection.subVectors(actor2.position, actor1.position);
+
+  //Normalize all vectors
+  direction1.normalize();
+  direction2.normalize();
+  relativeDirection.normalize();
+
+  //Calculate the dot products
+  const dotProduct1 = direction1.dot(relativeDirection);
+  const dotProduct2 = direction2.dot(relativeDirection);
+
+  // Compare dot products and return if they are facing eachother
+  return (dotProduct1 > 0 && dotProduct2 < 0);
+
+ 
 }
